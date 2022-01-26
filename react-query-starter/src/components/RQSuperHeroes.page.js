9f695cc03d6fd0e5eb4af25ae3574c3942773c1c
harsh-sharma-99 <sharmaharsh9944@gmail.com>
2022-01-26 16:27:53 +0530

    added custom react query hook

--- react-query-starter/src/components/RQSuperHeroes.page.js
+++ react-query-starter/src/components/RQSuperHeroes.page.js
@@ -1,39 +1,16 @@
-import { useQuery } from "react-query";
-import axios from "axios";
-import { useState } from "react";
-
-const fetchData = () => {
-  return axios.get("http://localhost:4000/superheroes");
-};
+import React from "react";
+import { useSuperHeroesData } from "../hooks/useSuperHerosData";
 
 export const RQSuperHeroesPage = () => {
-  const [interval, setInterval] = useState(3000);
-
   const onSuccess = (data) => {
     console.log("Perform side effect after data fetch", data);
-    if (data.length === 4) {
-      setInterval(false);
-      console.log("set flase");
-    }
   };
 
   const onError = (error) => {
     console.log("Perform side effect after encountering error", error);
   };
-  const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
-    "super-heroes",
-    fetchData,
-    {
-      refetchInterval: interval,
-      refetchIntervalInBackground: true,
-      onSuccess,
-      onError,
-      select: (data) => {
-        const superHeroNames = data?.data.map((hero) => hero.name);
-        return superHeroNames;
-      },
-    }
-  );
+  const { isLoading, data, isError, error, isFetching, refetch } =
+    useSuperHeroesData(onSuccess, onError);
 
   if (isLoading || isFetching) {
     return <h1>Loading ...</h1>;
@@ -47,9 +24,7 @@ export const RQSuperHeroesPage = () => {
     <>
       <h2>React Query Super Heroes Page</h2>
       <button onClick={refetch}>Call Heroes</button>
-      {/* {data?.data.map((hero) => {
-        return <div key={hero.name}>{hero.name}</div>;
-      })} */}
+
       {data?.map((heroName) => {
         return <div key={heroName}>{heroName}</div>;
       })}
